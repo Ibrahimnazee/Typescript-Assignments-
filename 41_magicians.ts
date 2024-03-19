484da17bb44e4e5627a1f20e9f5708c950b18aae
Ibrahimnazee <160228904+Ibrahimnazee@users.noreply.github.com>
2024-03-19 23:27:44 +0500

    Add files via upload

---
+++ 41_magicians.ts
@@ -0,0 +1,16 @@
+// Magicians: Make a array of magician’s names. Pass the array to a function called show_magicians(), which prints the name of each magician in the array.
+
+let magicians :string[] =['harry potter','finn allen','jeson jerry','michael charls'] 
+   function show_magicians(joker:string []){
+    
+    for (let joker of magicians){
+        console.log(joker);
+    }
+   }
+   show_magicians(magicians);
+
+
+
+
+
+
