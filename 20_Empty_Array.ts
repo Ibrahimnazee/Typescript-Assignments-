aaff1e98fea2464808297304e21b0b623322668d
Ibrahimnazee <160228904+Ibrahimnazee@users.noreply.github.com>
2024-03-19 23:05:41 +0500

    Add files via upload

---
+++ 20_Empty_Array.ts
@@ -0,0 +1,12 @@
+
+// Think of something you could store in aaray.
+let item :string [] =['Pakistan','Karachi','Urdu'];
+
+// write a program that creates a list containing these item.
+let item2 :string[] =[];
+
+item2.push('Autralia');
+item2.push('NewZealand');
+item2.push('Dubai');
+
+ console.log(item2);
