0286c3ec6020ae76f0b194e120ad699f60754a57
Ibrahimnazee <160228904+Ibrahimnazee@users.noreply.github.com>
2024-03-19 23:08:21 +0500

    Add files via upload

---
+++ 22_International_Error.ts
@@ -0,0 +1,11 @@
+
+// If you haven’t received an array index error in one of your programs yet, try to make one happen
+
+// Creating Array
+let newArray :string[] =['Shane Watson','Mike Hussy','Virat Kohli'];
+// Accessing Error
+console.log(newArray [5]);
+
+//  Make sure you correct the error before closing the program.
+// correct Array
+console.log(newArray);
