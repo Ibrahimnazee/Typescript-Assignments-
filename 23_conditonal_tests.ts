9580035e5f6ef4a3832cdf25cd88ce3cd5cb09b8
Ibrahimnazee <160228904+Ibrahimnazee@users.noreply.github.com>
2024-03-19 23:09:51 +0500

    Add files via upload

---
+++ 23_conditonal_tests.ts
@@ -0,0 +1,43 @@
+// Write a series of conditional tests.
+// create a variable
+ 
+let car :string = "subaru";
+// test numbner 1
+console.log("Is car === 'subaru'? I predict True.");
+console.log(car ==='subaru');
+
+// test number 2
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car =='subaru');
+
+// tets number 3
+console.log("Is car != 'honda'? I predict True.");
+console.log(car !='honda');
+
+// tets number 4
+console.log("Is car !== 'yamaha'? I predict True.");
+console.log(car !=='yamaha');
+
+// tets number 5
+console.log("Is car.Upper Case == 'SUBARU'? I predict True.");
+console.log(car.toUpperCase() =='SUBARU');
+
+// tets number 6
+console.log("Is car == 'yamaha'? I predict False.");
+console.log(car =='yamaha');
+
+// tets number 7
+console.log("Is car === 'Train'? I predict False.");
+console.log(car ==='Train');
+
+// tets number 8
+console.log("Is car === 'Bike'? I predict False.");
+console.log(car ==='Bike');
+
+// tets number 9
+console.log("Is car === 'cycle'? I predict False.");
+console.log(car ==='cycle');
+
+// tets number 10
+console.log("Is car.Lower Case == 'YAMAHA'? I predict False.");
+console.log(car.toLowerCase() =='YAMAHA');
