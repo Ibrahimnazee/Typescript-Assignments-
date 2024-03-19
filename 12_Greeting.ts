d3ccb4b12a3fe1d62e0ac1e9fd83e8aec8c3ca53
Ibrahimnazee <160228904+Ibrahimnazee@users.noreply.github.com>
2024-03-19 22:52:55 +0500

    Add files via upload

---
+++ 12_Greeting.ts
@@ -0,0 +1,9 @@
+
+let names :string[] =['ibrahim','sohail','ramzan','kashif'] ;
+
+const message :string = 'welcome to my typescript class: '
+
+for(let i=0; i<names.length; i++){
+
+    console.log(message + names[i]) ;
+}
